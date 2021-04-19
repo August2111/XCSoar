0b70d682968638db57bdf989e1b503dc17116c86
August2111 <Info@FlapsOnline.de>
2021-04-19 13:09:58 +0200

    CMake/WeGlide add weglide files to cmake
    add JsonMapParser for WeGlide

--- src/Cloud/CMakeSource.cmake
+++ src/Cloud/CMakeSource.cmake
@@ -2,7 +2,6 @@ set(Cloud_SOURCES
         Cloud/IGCFileUpload.cpp
         Cloud/weglide/FileToWeGlide.cpp
         Cloud/weglide/WeGlideSettings.cpp
-        Cloud/JsonParser.cpp
 )
 
 set(SCRIPT_FILES
---
+++ src/JSON/CMakeSource.cmake
@@ -0,0 +1,9 @@
+set(JSON_SOURCES
+        JSON/JsonMapParser.cpp
+)
+
+set(SCRIPT_FILES
+    CMakeSource.cmake
+)
+
+
