b35c1b1302fa0b6a2571bafe8b41db9d5c2a7dc6
August2111 <Info@FlapsOnline.de>
2021-07-05 21:45:43 +0200

    WeGlide Dialogs/DataField.cpp Redesign
    With switch case the data pathes is more clear and
    a cleaner separation between the types

--- src/Dialogs/DataField.cpp
+++ src/Dialogs/DataField.cpp
@@ -38,51 +38,71 @@ bool
 EditDataFieldDialog(const TCHAR *caption, DataField &df,
                     const TCHAR *help_text)
 {
-  if (df.GetType() == DataField::Type::FILE) {
-    return FilePicker(caption, (FileDataField &)df, help_text);
-  } else if (df.SupportsCombolist()) {
-    return ComboPicker(caption, df, help_text);
-  } else if (df.GetType() == DataField::Type::ROUGH_TIME) {
-    RoughTimeDataField &tdf = (RoughTimeDataField &)df;
+  switch (df.GetType()) {
+  case DataField::Type::FILE: {
+    return FilePicker(caption, (FileDataField&)df, help_text);
+  }
+  case DataField::Type::ROUGH_TIME: {
+    RoughTimeDataField& tdf = (RoughTimeDataField&)df;
     RoughTime value = tdf.GetValue();
     if (!TimeEntryDialog(caption, value, tdf.GetTimeZone(), true))
       return false;
 
     tdf.ModifyValue(value);
     return true;
-  } else if (df.GetType() == DataField::Type::GEOPOINT) {
-    GeoPointDataField &gdf = (GeoPointDataField &)df;
+  }
+  case DataField::Type::GEOPOINT: {
+    GeoPointDataField& gdf = (GeoPointDataField&)df;
     GeoPoint value = gdf.GetValue();
     if (!GeoPointEntryDialog(caption, value,
-                             gdf.GetFormat(),
-                             false))
+      gdf.GetFormat(),
+      false))
       return false;
 
     gdf.ModifyValue(value);
     return true;
-  } else if (df.GetType() == DataField::Type::DATE) {
-    DataFieldDate &dfd = reinterpret_cast<DataFieldDate &>(df);
+  }
+  case DataField::Type::DATE: {
+    DataFieldDate& dfd = reinterpret_cast<DataFieldDate&>(df);
     BrokenDate date = dfd.GetValue();
     if (!DateEntryDialog(caption, date, true))
       return false;
 
     dfd.SetValue(date);
     return true;
-  } else {
-    const TCHAR *value = df.GetAsString();
-    if (value == NULL)
-      return false;
+  }
+  case DataField::Type::INTEGER: {
+    if (df.SupportsCombolist()) {
+      return ComboPicker(caption, df, help_text);
+    } else {
+      unsigned int value = df.GetAsInteger();
+      if (!NumberEntryDialog(caption, value, 5))  // 5 columns!
+        return false;
+      df.SetAsInteger(value);
+      return true;
+    }
+  }
+  default: {
+    if (df.SupportsCombolist()) {
+      return ComboPicker(caption, df, help_text);
+    } else {
+      const TCHAR* value = df.GetAsString();
+      if (value == NULL)
+        return false;
 
-    StaticString<EDITSTRINGSIZE> buffer(value);
+      StaticString<EDITSTRINGSIZE> buffer(value);
 
-    PrefixDataField::AllowedCharactersFunction acf;
-    if (df.GetType() == DataField::Type::PREFIX)
-      acf = ((PrefixDataField &)df).GetAllowedCharactersFunction();
+      PrefixDataField::AllowedCharactersFunction acf;
+      if (df.GetType() == DataField::Type::PREFIX)
+        acf = ((PrefixDataField&)df).GetAllowedCharactersFunction();
 
-    if (!TextEntryDialog(buffer, caption, acf))
-      return false;
+      if (!TextEntryDialog(buffer, caption, acf))
+        return false;
 
-    df.SetAsString(buffer);
-    return true;
+      df.SetAsString(buffer);
+      return true;
+    }
+  }
   }
 }
+
