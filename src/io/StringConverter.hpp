e9052548d242ba9d947e98f400ceee52a5e58b52
August2111 <Info@FlapsOnline.de>
2021-07-12 10:17:32 +0200

    io/StringConverter.hpp - add 2 new functions with different call parameter definitions on StringConverter.Convert(..) ('const char *' and 'std::string')

--- src/io/StringConverter.hpp
+++ src/io/StringConverter.hpp
@@ -28,6 +28,7 @@ Copyright_License {
 #include "util/ReusableArray.hpp"
 
 #include <tchar.h>
+#include <string>
 
 /**
  * Helper which imports strings from a file to `TCHAR*`.
@@ -71,6 +72,12 @@ public:
    * Throws on error.
    */
   TCHAR *Convert(char *src);
+  inline TCHAR *Convert(const char *src) {
+    return Convert(const_cast<char*>(src));
+  }
+  inline TCHAR *Convert(std::string src) {
+    return Convert(const_cast<char*>(src.c_str()));
+  }
 };
 
 #endif
